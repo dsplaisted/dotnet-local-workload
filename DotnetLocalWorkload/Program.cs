31111cf3226297c841bfb3d24eda18bf692643ca
Daniel Plaisted <daplaist@microsoft.com>
2021-06-20 11:32:54 -0700

    Initial command parsing

---
+++ DotnetLocalWorkload/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+
+namespace DotnetLocalWorkload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+            };
+
+            var installCommand = new Command("install");
+            
+            var workloadArgument = new Argument("workload")
+            {
+                Arity = ArgumentArity.OneOrMore,
+            };
+            installCommand.AddArgument(workloadArgument);
+            rootCommand.Add(installCommand);
+
+            var parseResult = rootCommand.Parse(args);
+
+            if (parseResult.CommandResult.Command == installCommand)
+            {
+                var argumentResult = parseResult.ValueForArgument<List<string>>(workloadArgument);
+                Console.WriteLine("Workloads to install: " + string.Join(' ', argumentResult));
+            }
+            else
+            {
+                Console.WriteLine("Unknown command");
+            }
+            
+
+            
+        }
+    }
+}
